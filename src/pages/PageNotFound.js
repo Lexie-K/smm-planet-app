b313c29596c440190bb827addb195005c0ad9596
Lexie-K <xandra.kk@yandex.ru>
2023-05-11 22:35:12 +0300

    added page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
+import './styledPageNotFound.scss'
+import { Link } from 'react-router-dom';
 
-const PageNotFound = () => {
-  return <div>PageNotFound</div>;
-};
+function PageNotFound() {
+  return (
+    <div className="styledNotFound">
+      <h1>Oops! You might be lost</h1>
+
+      <img src={require('../assets/error.png')} alt="404 error" />
+      <p>This path leads only to the dark side...</p>
+      <Link to="/">Use teleport</Link>
+    </div>
+  );
+}
 
 export default PageNotFound;
